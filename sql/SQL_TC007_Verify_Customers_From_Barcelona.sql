34145eeeb71b09d9599379a3310764f360aa662c
Donnie <aureliodonniehanzelr7141@gmail.com>
2026-07-15 18:58:57 +0800

    Create SQL_TC007_Verify_Customers_From_Barcelona.sql

---
+++ sql/SQL_TC007_Verify_Customers_From_Barcelona.sql
@@ -0,0 +1,7 @@
+-- SQL_TC007: Verify customers from a specific city "Barcelona"
+
+SELECT c.customer_id, c.first_name, c.last_name, ci.city
+FROM customer c
+JOIN address a ON c.address_id = a.address_id
+JOIN city ci ON a.city_id = ci.city_id
+WHERE ci.city = 'Barcelona';
