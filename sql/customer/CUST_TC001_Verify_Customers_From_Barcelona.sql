0d8c876c3771bd11ececeb9c3d3ceba2cf4b6234
Donnie <aureliodonniehanzelr7141@gmail.com>
2026-07-17 12:35:14 +0800

    Update CUST_TC001_Verify_Customers_From_Barcelona.sql

--- sql/customer/CUST_TC001_Verify_Customers_From_Barcelona.sql
+++ sql/customer/CUST_TC001_Verify_Customers_From_Barcelona.sql
@@ -1,6 +1,10 @@
 -- CUST_TC001: Verify customers from a specific city "Barcelona"
 
-SELECT c.customer_id, c.first_name, c.last_name, ci.city
+SELECT c.customer_id, 
+       c.first_name, 
+       c.last_name, 
+       ci.city
+
 FROM customer c
 JOIN address a ON c.address_id = a.address_id
 JOIN city ci ON a.city_id = ci.city_id
