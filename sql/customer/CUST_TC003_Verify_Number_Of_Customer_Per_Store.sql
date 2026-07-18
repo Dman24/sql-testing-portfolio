39208fc25918c823fea57ad747849071ac38851a
Donnie <aureliodonniehanzelr7141@gmail.com>
2026-07-18 11:01:25 +0800

    Update CUST_TC003_Verify_Number_Of_Customer_Per_Store.sql

--- sql/customer/CUST_TC003_Verify_Number_Of_Customer_Per_Store.sql
+++ sql/customer/CUST_TC003_Verify_Number_Of_Customer_Per_Store.sql
@@ -1,13 +1,13 @@
 -- CUST_TC003: Verify number of customer per store
 
--- SQL Query 1 (Validation)
+-- Main Validation Query
 SELECT store_id,
 COUNT(customer_id) AS customer_count
 FROM customer
 GROUP BY store_id
 ORDER BY store_id;
 
--- SQL Query 2 (Verification)
+-- Supporting Verification Query
 SELECT customer_id,
 	   first_name,
        last_name,
