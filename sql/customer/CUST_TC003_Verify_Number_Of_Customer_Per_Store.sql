2203264671fae2d5e83251b0a973e774696a431b
Donnie <aureliodonniehanzelr7141@gmail.com>
2026-07-18 10:35:51 +0800

    Create CUST_TC003_Verify_Number_Of_Customer_Per_Store.sql

---
+++ sql/customer/CUST_TC003_Verify_Number_Of_Customer_Per_Store.sql
@@ -0,0 +1,16 @@
+-- CUST_TC003: Verify number of customer per store
+
+-- SQL Query 1 (Validation)
+SELECT store_id,
+COUNT(customer_id) AS customer_count
+FROM customer
+GROUP BY store_id
+ORDER BY store_id;
+
+-- SQL Query 2 (Verification)
+SELECT customer_id,
+	     first_name,
+       last_name,
+       store_id
+FROM customer
+ORDER BY store_id;
