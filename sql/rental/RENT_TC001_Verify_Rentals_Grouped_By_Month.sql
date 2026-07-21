dd01dd6a99424be44bf5d9dc5c7316b0d7d6afce
Donnie <aureliodonniehanzelr7141@gmail.com>
2026-07-21 20:10:28 +0800

    Update RENT_TC001_Verify_Rentals_Grouped_By_Month.sql

--- sql/rental/RENT_TC001_Verify_Rentals_Grouped_By_Month.sql
+++ sql/rental/RENT_TC001_Verify_Rentals_Grouped_By_Month.sql
@@ -1,8 +1,15 @@
 -- RENT_TC001: Verify rentals grouped by month
 
+-- Main Query
 SELECT 
   DATE_FORMAT(rental_date, '%Y-%m') AS rental_month,
   COUNT(*) AS rental_count
 FROM rental
 GROUP BY DATE_FORMAT(rental_date, '%Y-%m')
-ORDER BY rental_month
+ORDER BY rental_month;
+
+--Supporting Query
+SELECT rental_id,
+       rental_date
+FROM rental
+ORDER BY rental_date;
